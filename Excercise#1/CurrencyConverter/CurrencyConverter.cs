92f790f544a1841b55bcde5c416db1150e394b38
heyjhude-0 <jhudedagle@gmail.com>
2026-02-10 14:17:21 +0800

    added item no. 2

---
+++ Excercise#1/CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+Console.Write("Enter amount in USD: ");
+double amount = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+Console.Write("Enter exchange rate from USD to EUR: ");
+double rate = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+double result = amount * rate;
+
+Console.Write($"Amount in EUR: {result:F2}");
