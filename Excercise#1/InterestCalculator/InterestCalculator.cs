249d572e4f855880a2e143b35e8e5ed47e59899c
heyjhude-0 <jhudedagle@gmail.com>
2026-02-10 09:32:13 +0800

    initial commit

---
+++ Excercise#1/InterestCalculator/InterestCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+Console.Write("Enter the principal amount: ");
+double pAmount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Enter rate of interest: ");
+double rate = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Enter time in years: ");
+int time = Convert.ToInt32(Console.ReadLine());
+
+double result = (pAmount * rate * time) / 100;
+
+Console.WriteLine($"Simple Interest: {result} ");
